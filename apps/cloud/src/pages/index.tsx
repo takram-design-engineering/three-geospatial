8ae32a43602edd16c917af7508180dd68236d5ce
Shota Matsuda <shota@takram.com>
2024-09-12 15:07:48 +0900

    Remove styled root

--- apps/cloud/src/pages/index.tsx
+++ apps/cloud/src/pages/index.tsx
@@ -1,13 +1,7 @@
-import styled from '@emotion/styled'
 import { type NextPage } from 'next'
 
-const Root = styled.div`
-  width: 100%;
-  height: 100%;
-`
-
 const Index: NextPage = () => {
-  return <Root>Hello</Root>
+  return <>Hello</>
 }
 
 export default Index
