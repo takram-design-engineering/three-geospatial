a0acfadaf5dd18877c56b3c741c3a89281638324
Shota Matsuda <shota@takram.com>
2024-09-23 03:42:39 +0900

    Separate files

--- libs/atmosphere/src/usePrecomputedData.ts
+++ libs/atmosphere/src/usePrecomputedData.ts
@@ -15,20 +15,7 @@ import { type Constructor } from 'type-fest'
 
 import { assertType } from '@geovanni/core'
 
-function parseFloat32Array(buffer: ArrayBuffer): Float32Array {
-  const data = new DataView(buffer)
-  const array = new Float32Array(
-    data.byteLength / Float32Array.BYTES_PER_ELEMENT
-  )
-  for (
-    let index = 0, byteIndex = 0;
-    index < array.length;
-    ++index, byteIndex += Float32Array.BYTES_PER_ELEMENT
-  ) {
-    array[index] = data.getFloat32(byteIndex, true)
-  }
-  return array
-}
+import { parseFloat32Array } from './parseFloat32Array'
 
 interface ImageSize {
   width: number
