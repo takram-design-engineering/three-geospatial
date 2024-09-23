55841d51e2f52eed9a74f92c95e6232c5ea50b59
Shota Matsuda <shota@takram.com>
2024-09-23 17:40:27 +0900

    Move playground to container

--- apps/playground/src/components/ENUFrame.tsx
+++ apps/playground/src/components/ENUFrame.tsx
@@ -2,6 +2,8 @@ import { Ellipsoid } from '@math.gl/geospatial'
 import { createContext, useMemo, type FC, type ReactNode } from 'react'
 import { Matrix4, Quaternion } from 'three'
 
+type Position = [number, number, number]
+
 export const ENUFrameContext = createContext<Matrix4 | undefined>(undefined)
 
 export const ENUFrame: FC<{
@@ -10,7 +12,12 @@ export const ENUFrame: FC<{
   children?: ReactNode
 }> = ({ longitude, latitude, children }) => {
   const position = useMemo(
-    () => Ellipsoid.WGS84.cartographicToCartesian([longitude, latitude, 0]),
+    () =>
+      Ellipsoid.WGS84.cartographicToCartesian([
+        longitude,
+        latitude,
+        0
+      ]) as Position,
     [longitude, latitude]
   )
 
@@ -31,10 +38,7 @@ export const ENUFrame: FC<{
   )
 
   return (
-    <group
-      position={position as [number, number, number]}
-      quaternion={quaternion}
-    >
+    <group position={position} quaternion={quaternion}>
       <ENUFrameContext.Provider value={matrix}>
         {children}
       </ENUFrameContext.Provider>
