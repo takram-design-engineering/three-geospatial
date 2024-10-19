6e0b49664a7ed58a60073aa437112e65033bdb3a
Shota Matsuda <shota@takram.com>
2024-10-19 11:01:43 +0900

    Hide panel by default

--- libs/storybook/.storybook/manager.ts
+++ libs/storybook/.storybook/manager.ts
@@ -1,6 +1,7 @@
 import { addons } from '@storybook/manager-api'
 
 addons.setConfig({
-  panelPosition: 'bottom',
-  initialActive: 'canvas'
+  initialActive: 'canvas',
+  // https://github.com/storybookjs/storybook/issues/7149#issuecomment-2096502983
+  bottomPanelHeight: 0
 })
