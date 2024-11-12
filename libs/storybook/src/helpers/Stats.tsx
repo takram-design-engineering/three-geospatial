96d2d56eb7939a4b69d65c576b79e6e1fe7373e8
Shota Matsuda <shota@takram.com>
2024-11-13 05:32:15 +0900

    Add stats helper

---
+++ libs/storybook/src/helpers/Stats.tsx
@@ -0,0 +1,32 @@
+import { useFrame, useThree } from '@react-three/fiber'
+import { useControls } from 'leva'
+import { useEffect, useMemo, type FC } from 'react'
+import StatsImpl from 'stats-gl'
+
+export const Stats: FC = () => {
+  const { show } = useControls('stats', { show: false })
+
+  const gl = useThree(({ gl }) => gl)
+  const stats = useMemo(() => {
+    const stats = new StatsImpl()
+    void stats.init(gl)
+    return stats
+  }, [gl])
+
+  useEffect(() => {
+    if (show) {
+      document.body.appendChild(stats.dom)
+      return () => {
+        document.body.removeChild(stats.dom)
+      }
+    }
+  }, [show, stats])
+
+  useFrame(() => {
+    if (show) {
+      stats.update()
+    }
+  })
+
+  return null
+}
