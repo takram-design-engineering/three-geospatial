342ef08434b053fa60a56de79f9f55a9cef1ebff
Shota Matsuda <shota@takram.com>
2024-11-24 02:16:05 +0900

    Fix the order of smoothstep args

--- libs/core/src/math.ts
+++ libs/core/src/math.ts
@@ -4,13 +4,24 @@ export const clamp = MathUtils.clamp
 export const euclideanModulo = MathUtils.euclideanModulo
 export const inverseLerp = MathUtils.inverseLerp
 export const lerp = MathUtils.lerp
-export const smoothstep = MathUtils.smoothstep
 export const radians = MathUtils.degToRad
 export const degrees = MathUtils.radToDeg
 export const isPowerOfTwo = MathUtils.isPowerOfTwo
 export const ceilPowerOfTwo = MathUtils.ceilPowerOfTwo
 export const floorPowerOfTwo = MathUtils.floorPowerOfTwo
 export const normalize = MathUtils.normalize
+
+// Prefer glsl's argument order which differs from that of MathUtils.
+export function smoothstep(min: number, max: number, x: number): number {
+  if (x <= min) {
+    return 0
+  }
+  if (x >= max) {
+    return 1
+  }
+  x = (x - min) / (max - min)
+  return x * x * (3 - 2 * x)
+}
 
 export function saturate(x: number): number {
   return Math.min(Math.max(x, 0), 1)
