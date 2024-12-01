6c5adfbfee246c996c3a41a064f8d523752ed869
Shota Matsuda <shota@takram.com>
2024-12-01 12:32:52 +0900

    Add link to repository in storybook

--- storybook/.storybook/manager.ts
+++ storybook/.storybook/manager.ts
@@ -1,6 +1,8 @@
 import { addons } from '@storybook/manager-api'
+import theme from './theme'
 
 addons.setConfig({
+  theme,
   initialActive: 'canvas',
   // https://github.com/storybookjs/storybook/issues/7149#issuecomment-2096502983
   bottomPanelHeight: 0
---
+++ storybook/.storybook/theme.ts
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: '@takram/three-geospatial',
+  brandUrl: 'https://github.com/takram-design-engineering/three-geospatial/'
+})
