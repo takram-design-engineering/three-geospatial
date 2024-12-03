19ec9b5641b1308cb1c416dc3fc1554dd4d269ec
Shota Matsuda <shota@takram.com>
2024-12-03 12:13:40 +0900

    Remove lodash from bundle

--- packages/atmosphere/src/r3f/separateProps.ts
+++ packages/atmosphere/src/r3f/separateProps.ts
@@ -1,22 +1,34 @@
-import { omit, pick } from 'lodash-es'
-
 import { type AtmosphereMaterialProps } from '../AtmosphereMaterialBase'
-
-const propKeys = [
-  'irradianceTexture',
-  'scatteringTexture',
-  'transmittanceTexture',
-  'useHalfFloat',
-  'ellipsoid',
-  'correctAltitude',
-  'photometric',
-  'sunDirection',
-  'sunAngularRadius',
-  'renderTargetCount'
-] as const satisfies ReadonlyArray<keyof AtmosphereMaterialProps>
 
 export function separateProps<T extends AtmosphereMaterialProps>(
   params: T
 ): [AtmosphereMaterialProps, Omit<T, keyof AtmosphereMaterialProps>] {
-  return [pick(params, propKeys), omit(params, propKeys)]
+  const {
+    irradianceTexture,
+    scatteringTexture,
+    transmittanceTexture,
+    useHalfFloat,
+    ellipsoid,
+    correctAltitude,
+    photometric,
+    sunDirection,
+    sunAngularRadius,
+    renderTargetCount,
+    ...others
+  } = params
+  return [
+    {
+      irradianceTexture,
+      scatteringTexture,
+      transmittanceTexture,
+      useHalfFloat,
+      ellipsoid,
+      correctAltitude,
+      photometric,
+      sunDirection,
+      sunAngularRadius,
+      renderTargetCount
+    },
+    others
+  ]
 }
