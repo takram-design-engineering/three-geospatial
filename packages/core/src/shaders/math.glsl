1c8d1d5b235d6b09887c8f36edd82b11559bf99d
Shota Matsuda <shota@takram.com>
2024-12-13 09:37:49 +0900

    Overload float parameters

--- packages/core/src/shaders/math.glsl
+++ packages/core/src/shaders/math.glsl
@@ -14,24 +14,30 @@ vec4 inverseLerp(const vec4 x, const vec4 y, const vec4 a) {
   return (a - x) / (y - x);
 }
 
-float remap(
-  const float value,
-  const float min1,
-  const float max1,
-  const float min2,
-  const float max2
-) {
-  return min2 + (value - min1) / (max1 - min1) * (max2 - min2);
+float remap(const float x, const float min1, const float max1, const float min2, const float max2) {
+  return min2 + (x - min1) / (max1 - min1) * (max2 - min2);
 }
 
-vec2 remap(const vec2 value, const vec2 min1, const vec2 max1, const vec2 min2, const vec2 max2) {
-  return min2 + (value - min1) / (max1 - min1) * (max2 - min2);
+vec2 remap(const vec2 x, const vec2 min1, const vec2 max1, const vec2 min2, const vec2 max2) {
+  return min2 + (x - min1) / (max1 - min1) * (max2 - min2);
 }
 
-vec3 remap(const vec3 value, const vec3 min1, const vec3 max1, const vec3 min2, const vec3 max2) {
-  return min2 + (value - min1) / (max1 - min1) * (max2 - min2);
+vec2 remap(const vec2 x, const float min1, const float max1, const float min2, const float max2) {
+  return min2 + (x - min1) / (max1 - min1) * (max2 - min2);
 }
 
-vec4 remap(const vec4 value, const vec4 min1, const vec4 max1, const vec4 min2, const vec4 max2) {
-  return min2 + (value - min1) / (max1 - min1) * (max2 - min2);
+vec3 remap(const vec3 x, const vec3 min1, const vec3 max1, const vec3 min2, const vec3 max2) {
+  return min2 + (x - min1) / (max1 - min1) * (max2 - min2);
+}
+
+vec3 remap(const vec3 x, const float min1, const float max1, const float min2, const float max2) {
+  return min2 + (x - min1) / (max1 - min1) * (max2 - min2);
+}
+
+vec4 remap(const vec4 x, const vec4 min1, const vec4 max1, const vec4 min2, const vec4 max2) {
+  return min2 + (x - min1) / (max1 - min1) * (max2 - min2);
+}
+
+vec4 remap(const vec4 x, const float min1, const float max1, const float min2, const float max2) {
+  return min2 + (x - min1) / (max1 - min1) * (max2 - min2);
 }
