e99a1b1be37f01aa144ec66a4041f20fd0171409
Shota Matsuda <shota@takram.com>
2024-12-25 04:58:51 +0900

    Add typed array element type

--- packages/core/src/typedArray.ts
+++ packages/core/src/typedArray.ts
@@ -1,3 +1,5 @@
+import invariant from 'tiny-invariant'
+
 export type TypedArray =
   | Int8Array
   | Uint8Array
@@ -19,3 +21,33 @@ export type TypedArrayConstructor =
   | Uint32ArrayConstructor
   | Float32ArrayConstructor
   | Float64ArrayConstructor
+
+export type TypedArrayElementType =
+  | 'int8'
+  | 'uint8'
+  | 'int16'
+  | 'uint16'
+  | 'int32'
+  | 'uint32'
+  | 'float32'
+  | 'float64'
+
+export function getTypedArrayElementType(
+  array: TypedArray
+): TypedArrayElementType {
+  // prettier-ignore
+  const type = (
+    array instanceof Int8Array ? 'int8' :
+    array instanceof Uint8Array ? 'uint8' :
+    array instanceof Uint8ClampedArray ? 'uint8' :
+    array instanceof Int16Array ? 'int16' :
+    array instanceof Uint16Array ? 'uint16' :
+    array instanceof Int32Array ? 'int32' :
+    array instanceof Uint32Array ? 'uint32' :
+    array instanceof Float32Array ? 'float32' :
+    array instanceof Float64Array ? 'float64' :
+    null
+  )
+  invariant(type != null)
+  return type
+}
