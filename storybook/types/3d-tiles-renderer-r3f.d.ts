b6a4822ded12c7bc5b99243d3f877732d4151933
Shota Matsuda <shota@takram.com>
2024-12-30 09:43:39 +0900

    Add further type definitions of 3d-tiles-renderer/r3f

--- storybook/types/3d-tiles-renderer-r3f.d.ts
+++ storybook/types/3d-tiles-renderer-r3f.d.ts
@@ -1,40 +1,99 @@
 declare module '3d-tiles-renderer/r3f' {
-  import { type GlobeControls, type TilesRenderer } from '3d-tiles-renderer'
-  import { type FC, type RefAttributes } from 'react'
+  import {
+    type GlobeControls as GlobeControlsImpl,
+    type TilesRenderer as TilesRendererImpl,
+    type EnvironmentControls as EnvironmentControlsImpl,
+    type CameraTransitionManager,
+    type CameraTransitionMode
+  } from '3d-tiles-renderer'
+  import { type GroupProps } from '@react-three/fiber'
+  import {
+    type OrthographicCamera,
+    type PerspectiveCamera,
+    type Camera,
+    type Object3D
+  } from 'three'
+  import {
+    type ReactNode,
+    type Context,
+    type FC,
+    type RefAttributes
+  } from 'react'
+
+  export const TilesRendererContext: Context<TilesRendererImpl | null>
+
+  export interface EastNorthUpFrameProps {
+    lat?: number
+    lon?: number
+    height?: number
+    az?: number
+    el?: number
+    roll?: number
+    children?: ReactNode
+  }
+
+  export const EastNorthUpFrame: FC<EastNorthUpFrameProps>
+
+  export type TilesPluginProps<
+    Plugin extends new (...args: any[]) => any,
+    Params extends {} = ConstructorParameters<Plugin>[0] extends {}
+      ? ConstructorParameters<Plugin>[0]
+      : {}
+  > = Partial<Params> & {
+    plugin: Plugin
+    args?: Params | [Params]
+  }
 
   export function TilesPlugin<
-    T extends new (...args: any[]) => any,
-    Params extends {} = ConstructorParameters<T>[0] extends {}
-      ? ConstructorParameters<T>[0]
+    Plugin extends new (...args: any[]) => any,
+    Params extends {} = ConstructorParameters<Plugin>[0] extends {}
+      ? ConstructorParameters<Plugin>[0]
       : {}
   >(
-    props: {
-      args?: Params
-      plugin: T
-    } & Partial<Params> &
-      RefAttributes<T>
+    props: TilesPluginProps<Plugin, Params> & RefAttributes<Plugin>
   ): JSX.Element
 
-  export function TilesRenderer<
-    T extends new (...args: any[]) => any,
-    Params extends {} = ConstructorParameters<T>[0] extends {}
-      ? ConstructorParameters<T>[0]
-      : {}
-  >(
-    props: {
-      url?: string
-    } & Partial<Params> &
-      RefAttributes<TilesRenderer>
-  ): JSX.Element
+  export type TilesRendererProps = {
+    url?: string
+    group?: GroupProps
+    children?: ReactNode
+  } & Partial<TilesRendererImpl>
 
-  export function GlobeControls<
-    T extends new (...args: any[]) => any,
-    Params extends {} = ConstructorParameters<T>[0] extends {}
-      ? ConstructorParameters<T>[0]
-      : {}
-  >(props: {} & Partial<Params> & RefAttributes<GlobeControls>): JSX.Element
+  export const TilesRenderer: FC<
+    TilesRendererProps & RefAttributes<TilesRendererImpl>
+  >
 
-  export const CameraTransition: FC<{
-    mode: 'perspective' | 'orthographic'
-  }>
+  interface ControlsBaseComponentProps {
+    domElement?: HTMLCanvasElement | null
+    scene?: Object3D | null
+    camera?: Camera | null
+    tilesRenderer?: TilesRendererImpl | null
+  }
+
+  export interface EnvironmentControlsProps
+    extends ControlsBaseComponentProps,
+      Partial<EnvironmentControlsImpl> {}
+
+  export const EnvironmentControls: FC<
+    EnvironmentControlsProps & RefAttributes<EnvironmentControlsImpl>
+  >
+
+  export interface GlobeControlsProps
+    extends ControlsBaseComponentProps,
+      Partial<GlobeControlsImpl> {}
+
+  export const GlobeControls: FC<
+    GlobeControlsProps & RefAttributes<GlobeControlsImpl>
+  >
+
+  export interface CameraTransitionProps
+    extends Partial<InstanceType<CameraTransitionManager>> {
+    mode?: CameraTransitionMode
+    perspectiveCamera?: PerspectiveCamera
+    orthographicCamera?: OrthographicCamera
+  }
+
+  export const CameraTransition: FC<
+    CameraTransitionProps & RefAttributes<CameraTransitionManager>
+  >
 }
