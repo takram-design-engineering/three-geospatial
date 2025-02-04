e851b31f1f48381b7560c4a80ca230974e49ed75
Shota Matsuda <shota@takram.com>
2025-02-05 01:48:24 +0900

    Add default asset urls

---
+++ packages/clouds/src/constants.ts
@@ -0,0 +1,5 @@
+const ref = '04040ac514f7a2858322bfc1fce9a2bd84afd96b'
+export const DEFAULT_LOCAL_WEATHER_URL = `https://media.githubusercontent.com/media/takram-design-engineering/three-geospatial/${ref}/packages/clouds/assets/local_weather.png`
+export const DEFAULT_SHAPE_URL = `https://media.githubusercontent.com/media/takram-design-engineering/three-geospatial/${ref}/packages/clouds/assets/shape.bin`
+export const DEFAULT_SHAPE_DETAIL_URL = `https://media.githubusercontent.com/media/takram-design-engineering/three-geospatial/${ref}/packages/clouds/assets/shape_detail.bin`
+export const DEFAULT_TURBULENCE_URL = `https://media.githubusercontent.com/media/takram-design-engineering/three-geospatial/${ref}/packages/clouds/assets/turbulence.png`
