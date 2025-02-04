084ce416466924e3fc7acec8adad13786b166320
Shota Matsuda <shota@takram.com>
2025-02-05 03:45:43 +0900

    Add polymorphic texture props with default urls

--- packages/clouds/src/r3f/Clouds.tsx
+++ packages/clouds/src/r3f/Clouds.tsx
@@ -1,9 +1,31 @@
-import { useFrame } from '@react-three/fiber'
+import { useFrame, useThree } from '@react-three/fiber'
 import { EffectComposerContext } from '@react-three/postprocessing'
 import { useSetAtom } from 'jotai'
-import { forwardRef, useCallback, useContext, useEffect, useMemo } from 'react'
+import {
+  forwardRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from 'react'
+import {
+  LinearFilter,
+  LinearMipMapLinearFilter,
+  NoColorSpace,
+  RedFormat,
+  RepeatWrapping,
+  TextureLoader,
+  type Data3DTexture,
+  type Texture,
+  type WebGLRenderer
+} from 'three'
 
 import { AtmosphereContext, separateProps } from '@takram/three-atmosphere/r3f'
+import {
+  createData3DTextureLoaderClass,
+  parseUint8Array
+} from '@takram/three-geospatial'
 import {
   type ExpandNestedProps,
   type PassThoughInstanceProps
@@ -14,6 +36,14 @@ import {
   cloudsPassOptionsDefaults,
   type CloudsPassChangeEvent
 } from '../CloudsPass'
+import {
+  DEFAULT_LOCAL_WEATHER_URL,
+  DEFAULT_SHAPE_DETAIL_URL,
+  DEFAULT_SHAPE_URL,
+  DEFAULT_TURBULENCE_URL
+} from '../constants'
+import { type Procedural3DTexture } from '../Procedural3DTexture'
+import { type ProceduralTexture } from '../ProceduralTexture'
 import { CloudLayers, type CloudLayersChildren } from './CloudLayers'
 
 export type CloudsProps = Omit<
@@ -26,13 +56,97 @@ export type CloudsProps = Omit<
         ExpandNestedProps<CloudsPass, 'shadow'>
     >
   >,
-  'children'
+  | 'localWeatherTexture'
+  | 'shapeTexture'
+  | 'shapeDetailTexture'
+  | 'turbulenceTexture'
+  | 'children'
 > & {
   children?: CloudLayersChildren
+  localWeatherTexture?: Texture | ProceduralTexture | string
+  shapeTexture?: Data3DTexture | Procedural3DTexture | string
+  shapeDetailTexture?: Data3DTexture | Procedural3DTexture | string
+  turbulenceTexture?: Texture | ProceduralTexture | string
+}
+
+function useTextureState(
+  input: string | Texture | ProceduralTexture,
+  gl: WebGLRenderer
+): Texture | ProceduralTexture | null {
+  const [data, setData] = useState(typeof input !== 'string' ? input : null)
+  useEffect(() => {
+    if (typeof input === 'string') {
+      const loader = new TextureLoader()
+      ;(async () => {
+        const texture = await loader.loadAsync(input)
+        texture.minFilter = LinearMipMapLinearFilter
+        texture.magFilter = LinearFilter
+        texture.wrapS = RepeatWrapping
+        texture.wrapT = RepeatWrapping
+        texture.colorSpace = NoColorSpace
+        texture.needsUpdate = true
+
+        // WORKAROUND: Unless the texture is initialized here, the color space
+        // resets to sRGB for an unknown reason.
+        gl.initTexture(texture)
+
+        setData(texture)
+      })().catch(error => {
+        console.error(error)
+      })
+    } else {
+      setData(input)
+    }
+  }, [input, gl])
+
+  return data
+}
+
+function use3DTextureState(
+  input: string | Data3DTexture | Procedural3DTexture,
+  size: number
+): Data3DTexture | Procedural3DTexture | null {
+  const [data, setData] = useState(typeof input !== 'string' ? input : null)
+  useEffect(() => {
+    if (typeof input === 'string') {
+      const Loader = createData3DTextureLoaderClass(parseUint8Array, {
+        width: size,
+        height: size,
+        depth: size,
+        format: RedFormat,
+        minFilter: LinearFilter,
+        magFilter: LinearFilter,
+        wrapS: RepeatWrapping,
+        wrapT: RepeatWrapping,
+        wrapR: RepeatWrapping,
+        colorSpace: NoColorSpace
+      })
+      const loader = new Loader()
+      ;(async () => {
+        setData(await loader.loadAsync(input))
+      })().catch(error => {
+        console.error(error)
+      })
+    } else {
+      setData(input)
+    }
+  }, [input, size])
+
+  return data
 }
 
 export const Clouds = /*#__PURE__*/ forwardRef<CloudsPass, CloudsProps>(
-  function Clouds({ children, ...props }, forwardedRef) {
+  function Clouds(
+    {
+      localWeatherTexture: localWeatherTextureProp = DEFAULT_LOCAL_WEATHER_URL,
+      shapeTexture: shapeTextureProp = DEFAULT_SHAPE_URL,
+      shapeDetailTexture: shapeDetailTextureProp = DEFAULT_SHAPE_DETAIL_URL,
+      turbulenceTexture: turbulenceTextureProp = DEFAULT_TURBULENCE_URL,
+      children,
+      ...props
+    },
+    forwardedRef
+  ) {
     const { textures, stbn, transientStates, atoms, ...contextProps } =
       useContext(AtmosphereContext)
 
@@ -42,9 +156,6 @@ export const Clouds = /*#__PURE__*/ forwardRef<CloudsPass, CloudsProps>(
       ...textures,
       ...props
     })
-
-    const context = useContext(EffectComposerContext)
-    const { camera } = context
 
     const pass = useMemo(() => new CloudsPass(), [])
     useEffect(() => {
@@ -94,6 +205,13 @@ export const Clouds = /*#__PURE__*/ forwardRef<CloudsPass, CloudsProps>(
       }
     }, [pass, handleChange])
 
+    const gl = useThree(({ gl }) => gl)
+    const localWeatherTexture = useTextureState(localWeatherTextureProp, gl)
+    const shapeTexture = use3DTextureState(shapeTextureProp, 128)
+    const shapeDetailTexture = use3DTextureState(shapeDetailTextureProp, 32)
+    const turbulenceTexture = useTextureState(turbulenceTextureProp, gl)
+
+    const { camera } = useContext(EffectComposerContext)
     return (
       <>
         <primitive
@@ -101,6 +219,10 @@ export const Clouds = /*#__PURE__*/ forwardRef<CloudsPass, CloudsProps>(
           object={pass}
           mainCamera={camera}
           {...atmosphereParameters}
+          localWeatherTexture={localWeatherTexture}
+          shapeTexture={shapeTexture}
+          shapeDetailTexture={shapeDetailTexture}
+          turbulenceTexture={turbulenceTexture}
           stbnTexture={stbn}
           {...others}
         />
