6d1472b1b5d21d9378f277ae12d74d90d8137093
Shota Matsuda <shota@takram.com>
2025-02-08 02:49:14 +0900

    Rewrite useCloudsControls using shorthand and component props

--- storybook/src/clouds/Clouds.stories.tsx
+++ storybook/src/clouds/Clouds.stories.tsx
@@ -1,9 +1,8 @@
 import { type Meta } from '@storybook/react'
 
 import _Basic from './Clouds-Basic'
-import _Layers from './Clouds-Layers'
+// import _Layers from './Clouds-Layers'
 import _MovingEllipsoid from './Clouds-MovingEllipsoid'
-import _QualityPresets from './Clouds-QualityPresets'
 
 export default {
   title: 'clouds (WIP)/Clouds',
@@ -13,6 +12,5 @@ export default {
 } satisfies Meta
 
 export const Basic = _Basic
-export const QualityPresets = _QualityPresets
-export const Layers = _Layers
+// export const Layers = _Layers
 export const MovingEllipsoid = _MovingEllipsoid
