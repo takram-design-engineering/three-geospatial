82eb1ef921b3f2ac28aca638cb43f0366b7f1997
Shota Matsuda <shota@takram.com>
2025-02-16 04:13:00 +0900

    Minimal is more natural

--- storybook/src/clouds/MinimalSetup.stories.tsx
+++ storybook/src/clouds/MinimalSetup.stories.tsx
@@ -4,16 +4,17 @@ import { type Meta, type StoryFn } from '@storybook/react'
 import { ToneMappingMode } from 'postprocessing'
 
 import { AerialPerspective, Atmosphere } from '@takram/three-atmosphere/r3f'
+import { Clouds } from '@takram/three-clouds/r3f'
 import { LensFlare } from '@takram/three-geospatial-effects/r3f'
 
 export default {
-  title: 'atmosphere/Minimum Setup',
+  title: 'clouds (WIP)/Minimal Setup',
   parameters: {
     layout: 'fullscreen'
   }
 } satisfies Meta
 
-export const MinimumSetup: StoryFn = () => (
+export const MinimalSetup: StoryFn = () => (
   <Canvas
     gl={{
       antialias: false,
@@ -22,13 +23,16 @@ export const MinimumSetup: StoryFn = () => (
       toneMappingExposure: 10
     }}
     camera={{
-      // See the Sky/Basic story for deriving ECEF coordinates and rotation.
-      position: [5232062.795055689, -2.8678862431699113, 3639017.417296496],
-      rotation: [0.7072729447236096, -0.48911705050206433, -1.1888907679219152]
+      near: 1,
+      far: 4e5,
+      // See the Clouds/Basic story for deriving ECEF coordinates and rotation.
+      position: [4529893.894855564, 2615333.425024031, 3638042.815326614],
+      rotation: [0.6423512931563148, -0.2928348796035058, -0.8344824769956042]
     }}
   >
-    <Atmosphere date={Date.parse('2025-01-01T09:00:00Z')}>
+    <Atmosphere date={Date.parse('2025-01-01T07:00:00Z')}>
       <EffectComposer multisampling={0} enableNormalPass>
+        <Clouds />
         <AerialPerspective sky sunIrradiance skyIrradiance />
         <LensFlare />
         <ToneMapping mode={ToneMappingMode.AGX} />
