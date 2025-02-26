3df280a34215dae8371f61d9bcbf6f1fbe6deedd
Shota Matsuda <shota@takram.com>
2025-02-26 11:18:00 +0900

    Fix incorrect decoding of negative z normals

--- packages/terrain-core/src/decodeOctNormal.ts
+++ packages/terrain-core/src/decodeOctNormal.ts
@@ -4,6 +4,7 @@ function signNotZero(value: number): number {
   return value >= 0 ? 1 : -1
 }
 
+// Reference: https://github.com/CesiumGS/cesium/blob/1.122/packages/engine/Source/Core/AttributeCompression.js#L119
 export function decodeOctNormal(
   octX: number,
   octY: number,
@@ -15,10 +16,11 @@ export function decodeOctNormal(
   }
   let x = (octX / range) * 2 - 1
   let y = (octY / range) * 2 - 1
-  const z = 1 - Math.abs(x) - Math.abs(y)
+  const z = 1 - (Math.abs(x) + Math.abs(y))
   if (z < 0) {
-    x = (1 - Math.abs(y)) * signNotZero(x)
-    y = (1 - Math.abs(x)) * signNotZero(y)
+    const oldX = x
+    x = (1 - Math.abs(y)) * signNotZero(oldX)
+    y = (1 - Math.abs(oldX)) * signNotZero(y)
   }
   return result.set(x, y, z).normalize()
 }
