7a6d5afcf85248a6349b0bca6667c885a490992e
Shota Matsuda <shota@takram.com>
2025-03-01 13:40:41 +0900

    Add support for sampling half-float textures

--- packages/core/src/typedArray.ts
+++ packages/core/src/typedArray.ts
@@ -61,3 +61,18 @@ export function getTypedArrayElementType(
   invariant(type != null)
   return type
 }
+
+export function isTypedArray(value: unknown): value is TypedArray {
+  return (
+    value instanceof Int8Array ||
+    value instanceof Uint8Array ||
+    value instanceof Uint8ClampedArray ||
+    value instanceof Int16Array ||
+    value instanceof Uint16Array ||
+    value instanceof Int32Array ||
+    value instanceof Uint32Array ||
+    value instanceof Float16Array ||
+    value instanceof Float32Array ||
+    value instanceof Float64Array
+  )
+}
