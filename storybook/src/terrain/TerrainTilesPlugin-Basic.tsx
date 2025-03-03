099703308190b74688518be84708b7f9f2269546
Shota Matsuda <shota@takram.com>
2025-03-03 19:33:36 +0900

    Move camera near the surface

--- storybook/src/terrain/TerrainTilesPlugin-Basic.tsx
+++ storybook/src/terrain/TerrainTilesPlugin-Basic.tsx
@@ -1,6 +1,6 @@
 /// <reference types="vite/types/importMeta.d.ts" />
 
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { type StoryFn } from '@storybook/react'
 import {
   CesiumIonAuthPlugin,
@@ -12,12 +12,29 @@ import {
   TilesPlugin,
   TilesRenderer
 } from '3d-tiles-renderer/r3f'
-import { type FC } from 'react'
+import { useLayoutEffect, type FC } from 'react'
 import { MeshNormalMaterial } from 'three'
 
 import { TerrainTilesPlugin } from '@takram/three-3d-tiles-support'
+import { Geodetic, PointOfView, radians } from '@takram/three-geospatial'
+
+const longitude = 138.5973
+const latitude = 35.2138
+const heading = 71
+const pitch = -31
+const distance = 7000
 
 const Globe: FC = () => {
+  const camera = useThree(({ camera }) => camera)
+  useLayoutEffect(() => {
+    new PointOfView(distance, radians(heading), radians(pitch)).decompose(
+      new Geodetic(radians(longitude), radians(latitude)).toECEF(),
+      camera.position,
+      camera.quaternion,
+      camera.up
+    )
+  }, [camera])
+
   return (
     <TilesRenderer>
       <TilesPlugin
@@ -41,12 +58,7 @@ const Globe: FC = () => {
 
 const Story: StoryFn = () => {
   return (
-    <Canvas
-      camera={{
-        position: [0, -1e7, 1e7],
-        up: [0, 0, 1]
-      }}
-    >
+    <Canvas>
       <Globe />
     </Canvas>
   )
