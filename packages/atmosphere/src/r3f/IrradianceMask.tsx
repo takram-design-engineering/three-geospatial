1035f4d6a1138d6238e400b6491c98b70c29a31a
Shota Matsuda <shota@takram.com>
2025-06-05 05:39:21 +0900

    Add irradiance mask pass and corresponding component

---
+++ packages/atmosphere/src/r3f/IrradianceMask.tsx
@@ -0,0 +1,52 @@
+import { extend, type ThreeElement } from '@react-three/fiber'
+import { EffectComposerContext } from '@react-three/postprocessing'
+import {
+  forwardRef,
+  useContext,
+  useLayoutEffect,
+  useRef,
+  type ComponentPropsWithRef
+} from 'react'
+import { mergeRefs } from 'react-merge-refs'
+
+import { type ExpandNestedProps } from '@takram/three-geospatial/r3f'
+
+import { IrradianceMaskPass } from '../IrradianceMaskPass'
+import { AtmosphereContext } from './Atmosphere'
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    irradianceMaskPass: ThreeElement<typeof IrradianceMaskPass>
+  }
+}
+
+export interface IrradianceMaskProps
+  extends Omit<ComponentPropsWithRef<'irradianceMaskPass'>, 'args'>,
+    Partial<ExpandNestedProps<IrradianceMaskPass, 'selection'>> {}
+
+export const IrradianceMask = forwardRef<
+  IrradianceMaskPass,
+  IrradianceMaskProps
+>(function IrradianceMask(props, forwardedRef) {
+  const { transientStates } = useContext(AtmosphereContext)
+  const ref = useRef<IrradianceMaskPass>(null)
+
+  useLayoutEffect(() => {
+    if (transientStates != null) {
+      transientStates.irradianceMaskBuffer = ref.current?.texture ?? null
+      return () => {
+        transientStates.irradianceMaskBuffer = null
+      }
+    }
+  }, [transientStates])
+
+  const { scene, camera } = useContext(EffectComposerContext)
+  extend({ IrradianceMaskPass })
+  return (
+    <irradianceMaskPass
+      ref={mergeRefs([ref, forwardedRef])}
+      {...props}
+      args={[scene, camera]}
+    />
+  )
+})
