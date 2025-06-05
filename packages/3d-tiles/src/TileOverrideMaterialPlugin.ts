bb7232b27991606f85c8d6f71db7fad65d11eb7d
Shota Matsuda <shota@takram.com>
2025-06-05 16:51:59 +0900

    Clean up event listeners on disposal

--- packages/3d-tiles/src/TileOverrideMaterialPlugin.ts
+++ packages/3d-tiles/src/TileOverrideMaterialPlugin.ts
@@ -1,4 +1,7 @@
-import { type TilesRenderer } from '3d-tiles-renderer'
+import {
+  type TilesRenderer,
+  type TilesRendererEventMap
+} from '3d-tiles-renderer'
 import { Mesh, type Material } from 'three'
 
 export interface TileOverrideMaterialPluginOptions {
@@ -7,22 +10,40 @@ export interface TileOverrideMaterialPluginOptions {
 
 export class TileOverrideMaterialPlugin {
   readonly options: TileOverrideMaterialPluginOptions
+  tiles?: TilesRenderer
 
   constructor(options?: TileOverrideMaterialPluginOptions) {
     this.options = { ...options }
   }
 
+  private readonly handleTileVisibilityChange = ({
+    scene,
+    visible
+  }: TilesRendererEventMap['tile-visibility-change']): void => {
+    if (visible && scene instanceof Mesh) {
+      scene.material = this.options.material
+    }
+  }
+
   // Plugin method
   init(tiles: TilesRenderer): void {
+    this.tiles = tiles
     tiles.group.traverse(object => {
       if (object instanceof Mesh) {
         object.material = this.options.material
       }
     })
-    tiles.addEventListener('tile-visibility-change', ({ scene, visible }) => {
-      if (visible && scene instanceof Mesh) {
-        scene.material = this.options.material
-      }
-    })
+    tiles.addEventListener(
+      'tile-visibility-change',
+      this.handleTileVisibilityChange
+    )
+  }
+
+  // Plugin method
+  dispose(): void {
+    this.tiles?.removeEventListener(
+      'tile-visibility-change',
+      this.handleTileVisibilityChange
+    )
   }
 }
