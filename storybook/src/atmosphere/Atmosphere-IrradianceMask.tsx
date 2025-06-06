56f36f4a9372d2240991e791358e5c42438016d3
Shota Matsuda <shota@takram.com>
2025-06-06 17:06:04 +0900

    Rewrite the irradiance mask story with an ISS model

--- storybook/src/atmosphere/Atmosphere-IrradianceMask.tsx
+++ storybook/src/atmosphere/Atmosphere-IrradianceMask.tsx
@@ -1,78 +1,72 @@
 /// <reference types="vite/types/importMeta.d.ts" />
 
+import { css } from '@emotion/react'
 import {
   OrbitControls,
   RenderCubeTexture,
-  TorusKnot,
+  useGLTF,
   type RenderCubeTextureApi
 } from '@react-three/drei'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { SMAA, ToneMapping } from '@react-three/postprocessing'
 import { type StoryFn } from '@storybook/react-vite'
-import { CesiumIonAuthPlugin } from '3d-tiles-renderer/plugins'
-import { TilesPlugin, TilesRenderer } from '3d-tiles-renderer/r3f'
 import {
-  Fragment,
-  useEffect,
-  useRef,
-  useState,
-  type ComponentRef,
-  type FC
-} from 'react'
-import { Layers, MeshBasicMaterial, type Group } from 'three'
+  GLTFExtensionsPlugin,
+  GoogleCloudAuthPlugin,
+  ReorientationPlugin,
+  UpdateOnChangePlugin
+} from '3d-tiles-renderer/plugins'
+import {
+  TilesAttributionOverlay,
+  TilesPlugin,
+  TilesRenderer
+} from '3d-tiles-renderer/r3f'
+import { Fragment, useEffect, useRef, useState, type FC } from 'react'
+import { Layers, Vector3, type Group } from 'three'
+import { DRACOLoader } from 'three-stdlib'
 
+import { type AerialPerspectiveEffect } from '@takram/three-atmosphere'
 import {
   AerialPerspective,
   Atmosphere,
   IrradianceMask,
   Sky,
   SkyLight,
-  Stars,
   SunLight,
   type AtmosphereApi
 } from '@takram/three-atmosphere/r3f'
-import {
-  Ellipsoid,
-  Geodetic,
-  PointOfView,
-  radians
-} from '@takram/three-geospatial'
-import {
-  Depth,
-  Dithering,
-  LensFlare,
-  Normal
-} from '@takram/three-geospatial-effects/r3f'
-import { EastNorthUpFrame, EllipsoidMesh } from '@takram/three-geospatial/r3f'
+import { Ellipsoid, Geodetic, radians } from '@takram/three-geospatial'
+import { Dithering, LensFlare } from '@takram/three-geospatial-effects/r3f'
 
 import { EffectComposer } from '../helpers/EffectComposer'
 import { Stats } from '../helpers/Stats'
 import { useControls } from '../helpers/useControls'
 import { useLocalDateControls } from '../helpers/useLocalDateControls'
 import { useToneMappingControls } from '../helpers/useToneMappingControls'
-import { TileOverrideMaterialPlugin } from '../plugins/TileOverrideMaterialPlugin'
+import { TileCreasedNormalsPlugin } from '../plugins/TileCreasedNormalsPlugin'
 
-const geodetic = new Geodetic(radians(138.5), radians(36.2), 5000)
+const longitude = -110
+const latitude = 45
+const height = 408000
+const geodetic = new Geodetic(radians(longitude), radians(latitude), height)
 const position = geodetic.toECEF()
-
-const terrainMaterial = new MeshBasicMaterial({ color: 'gray' })
+const east = new Vector3()
+const north = new Vector3()
+const up = new Vector3()
 
 const IRRADIANCE_MASK_LAYER = 10
 const layers = new Layers()
 layers.enable(IRRADIANCE_MASK_LAYER)
 
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
+
 const Scene: FC = () => {
-  const { toneMappingMode } = useToneMappingControls({ exposure: 10 })
-  const { lensFlare, normal, depth } = useControls(
-    'effects',
-    {
-      lensFlare: true,
-      depth: false,
-      normal: false
-    },
-    { collapsed: true }
-  )
-  const motionDate = useLocalDateControls()
+  const { toneMappingMode } = useToneMappingControls({ exposure: 8 })
+  const motionDate = useLocalDateControls({
+    longitude,
+    timeOfDay: 19
+  })
   const { correctAltitude, photometric } = useControls(
     'atmosphere',
     {
@@ -81,161 +75,159 @@ const Scene: FC = () => {
     },
     { collapsed: true }
   )
-  const { enabled, transmittance, inscatter } = useControls(
-    'aerial perspective',
+  const { useEnvMap, showIrradianceMask, disableMask } = useControls(
+    'rendering',
     {
-      enabled: true,
-      transmittance: true,
-      inscatter: true
+      useEnvMap: true,
+      showIrradianceMask: false,
+      disableMask: false
     }
   )
-  const { sun, sky } = useControls('lighting', {
-    sun: true,
-    sky: true
-  })
-  const {
-    albedo,
-    metalness,
-    roughness,
-    clearcoat,
-    envMap: useEnvMap
-  } = useControls('material', {
-    albedo: { value: 0, min: 0, max: 1 },
-    metalness: { value: 0, min: 0, max: 1 },
-    roughness: { value: 0, min: 0, max: 1 },
-    clearcoat: { value: 1, min: 0, max: 1 },
-    envMap: true
-  })
 
-  const { camera } = useThree()
-  const [controls, setControls] = useState<ComponentRef<
-    typeof OrbitControls
-  > | null>(null)
-
+  const [atmosphere, setAtmosphere] = useState<AtmosphereApi | null>(null)
   useEffect(() => {
-    const pov = new PointOfView(2000, radians(-90), radians(-20))
-    pov.decompose(position, camera.position, camera.quaternion, camera.up)
-    if (controls != null) {
-      controls.target.copy(position)
-      controls.update()
-    }
-  }, [camera, controls])
-
-  const atmosphereRef = useRef<AtmosphereApi>(null)
-  useFrame(() => {
-    const atmosphere = atmosphereRef.current
     if (atmosphere == null) {
       return
     }
-    atmosphere.updateByDate(new Date(motionDate.get()))
-    envMapParentRef.current?.position.copy(position)
+    // Offset the ellipsoid so that the world space origin locates at the
+    // position relative to the ellipsoid.
+    geodetic.set(radians(longitude), radians(latitude), height)
+    geodetic.toECEF(position)
+    atmosphere.ellipsoidCenter.copy(position).multiplyScalar(-1)
+
+    // Rotate the ellipsoid around the world space origin so that the camera's
+    // orientation aligns with X: north, Y: up, Z: east, for example.
+    Ellipsoid.WGS84.getEastNorthUpVectors(position, east, north, up)
+    atmosphere.ellipsoidMatrix.makeBasis(north, up, east).invert()
+  }, [atmosphere])
+
+  useFrame(() => {
+    if (atmosphere != null) {
+      atmosphere.updateByDate(new Date(motionDate.get()))
+    }
   })
 
   const envMapParentRef = useRef<Group>(null)
   const [envMap, setEnvMap] = useState<RenderCubeTextureApi | null>(null)
+  const scene = useThree(({ scene }) => scene)
+  useEffect(() => {
+    scene.environment = useEnvMap ? (envMap?.fbo.texture ?? null) : null
+  }, [useEnvMap, envMap, scene])
+
+  const iss = useGLTF('assets/iss.glb')
+  useEffect(() => {
+    iss.scene.traverse(object => {
+      object.layers = layers
+      object.receiveShadow = true
+      object.castShadow = true
+    })
+  }, [iss])
+
+  const effectRef = useRef<AerialPerspectiveEffect>(null)
+  useEffect(() => {
+    const effect = effectRef.current
+    if (effect == null) {
+      return
+    }
+    if (showIrradianceMask) {
+      effect.defines.set('DEBUG_SHOW_IRRADIANCE_MASK', '1')
+    } else {
+      effect.defines.delete('DEBUG_SHOW_IRRADIANCE_MASK')
+    }
+    ;(effect as any).setChanged()
+  }, [showIrradianceMask])
+
   return (
     <Atmosphere
-      ref={atmosphereRef}
+      ref={setAtmosphere}
       textures='atmosphere'
       correctAltitude={correctAltitude}
       photometric={photometric}
     >
-      <OrbitControls ref={setControls} />
+      <OrbitControls />
 
       {/* Background objects and light sources */}
       <Sky />
-      <Stars data='atmosphere/stars.bin' />
-      <group position={position}>
-        {sun && <SunLight />}
-        {sky && <SkyLight />}
-      </group>
-
-      {/* An ellipsoid mesh for fill empty region */}
-      <EllipsoidMesh args={[Ellipsoid.WGS84.radii, 360, 180]}>
-        <meshBasicMaterial color='gray' />
-      </EllipsoidMesh>
+      {/* TODO: <Stars data='atmosphere/stars.bin' /> */}
+      <SunLight
+        distance={80}
+        castShadow
+        shadow-normalBias={1}
+        shadow-mapSize={[2048, 2048]}
+      >
+        <orthographicCamera
+          attach='shadow-camera'
+          top={60}
+          bottom={-60}
+          left={-60}
+          right={60}
+          near={0}
+          far={160}
+        />
+      </SunLight>
+      {/* Sky light must be turned off when using an environment map. */}
+      {!useEnvMap && <SkyLight />}
 
       {/* Quantized mesh terrain */}
-      <TilesRenderer>
+      <TilesRenderer
+        // The root URL sometimes becomes null without specifying the URL.
+        url={`https://tile.googleapis.com/v1/3dtiles/root.json?key=${import.meta.env.STORYBOOK_GOOGLE_MAP_API_KEY}`}
+      >
         <TilesPlugin
-          plugin={CesiumIonAuthPlugin}
+          plugin={GoogleCloudAuthPlugin}
           args={{
-            apiToken: import.meta.env.STORYBOOK_ION_API_TOKEN,
-            assetId: 2767062, // Japan Regional Terrain
+            apiToken: import.meta.env.STORYBOOK_GOOGLE_MAP_API_KEY,
             autoRefreshToken: true
           }}
         />
+        <TilesPlugin plugin={GLTFExtensionsPlugin} dracoLoader={dracoLoader} />
         <TilesPlugin
-          plugin={TileOverrideMaterialPlugin}
-          args={{ material: terrainMaterial }}
+          plugin={TileCreasedNormalsPlugin}
+          args={{ creaseAngle: radians(30) }}
         />
+        <TilesPlugin plugin={UpdateOnChangePlugin} />
+        <TilesPlugin
+          plugin={ReorientationPlugin}
+          args={{
+            lon: radians(longitude),
+            lat: radians(latitude),
+            height
+          }}
+        />
+        <TilesAttributionOverlay />
       </TilesRenderer>
 
       {/* Scene objects in a ENU frame */}
-      <EastNorthUpFrame {...geodetic}>
-        <TorusKnot
-          args={[200, 60, 256, 64]}
-          position={[0, 0, 20]}
-          layers={layers}
-        >
-          <meshPhysicalMaterial
-            color={[albedo, albedo, albedo]}
-            metalness={metalness}
-            roughness={roughness}
-            clearcoat={clearcoat}
-            envMap={useEnvMap ? envMap?.fbo.texture : null}
-          />
-        </TorusKnot>
-      </EastNorthUpFrame>
+      <group rotation-x={-Math.PI / 2}>
+        <primitive
+          object={iss.scene}
+          scale={1}
+          rotation-x={Math.PI / 2}
+          rotation-y={Math.PI / 2}
+        />
+      </group>
 
       {/* Off-screen environment map */}
-      {useEnvMap && (
-        <group ref={envMapParentRef}>
-          <RenderCubeTexture ref={setEnvMap} resolution={64}>
-            <Sky
-              sun={sun}
-              groundAlbedo='gray'
-              // Increase this to avoid flickers. Total radiance doesn't change.
-              sunAngularRadius={0.1}
-            />
-          </RenderCubeTexture>
-        </group>
-      )}
+      <group ref={envMapParentRef}>
+        <RenderCubeTexture ref={setEnvMap} resolution={64}>
+          <Sky
+            // Turn off the sun because we already have a sun directional light.
+            sun={false}
+          />
+        </RenderCubeTexture>
+      </group>
 
       {/* Post-processing */}
-      <EffectComposer multisampling={0}>
-        <Fragment
-          // Effects are order-dependant; we need to reconstruct the nodes.
-          key={JSON.stringify([
-            enabled,
-            sun,
-            sky,
-            transmittance,
-            inscatter,
-            lensFlare,
-            normal,
-            depth
-          ])}
-        >
-          <IrradianceMask selectionLayer={IRRADIANCE_MASK_LAYER} />
-          {enabled && !normal && !depth && (
-            <AerialPerspective
-              sunIrradiance={sun}
-              skyIrradiance={sky}
-              transmittance={transmittance}
-              inscatter={inscatter}
-            />
+      <EffectComposer multisampling={0} enableNormalPass>
+        <Fragment key={JSON.stringify([disableMask])}>
+          {!disableMask && (
+            <IrradianceMask selectionLayer={IRRADIANCE_MASK_LAYER} />
           )}
-          {lensFlare && <LensFlare />}
-          {depth && <Depth useTurbo />}
-          {normal && <Normal />}
-          {!normal && !depth && (
-            <>
-              <ToneMapping mode={toneMappingMode} />
-              <SMAA />
-              <Dithering />
-            </>
-          )}
+          <AerialPerspective ref={effectRef} sunIrradiance skyIrradiance />
+          <LensFlare />
+          <ToneMapping mode={toneMappingMode} />
+          <SMAA />
+          <Dithering />
         </Fragment>
       </EffectComposer>
     </Atmosphere>
@@ -243,16 +235,43 @@ const Scene: FC = () => {
 }
 
 const Story: StoryFn = () => (
-  <Canvas
-    gl={{
-      depth: false,
-      logarithmicDepthBuffer: true
-    }}
-    camera={{ near: 100, far: 1e6 }}
-  >
-    <Stats />
-    <Scene />
-  </Canvas>
+  <>
+    <Canvas
+      gl={{
+        depth: false,
+        logarithmicDepthBuffer: true
+      }}
+      camera={{ position: [80, 30, 100], near: 10, far: 1e7, fov: 40 }}
+      shadows
+    >
+      <Stats />
+      <Scene />
+    </Canvas>
+    <div
+      css={css`
+        position: absolute;
+        bottom: 16px;
+        right: 16px;
+        color: white;
+        font-size: small;
+        letter-spacing: 0.025em;
+      `}
+    >
+      Model:{' '}
+      <a
+        href='https://science.nasa.gov/resource/international-space-station-3d-model/'
+        target='_blank'
+        rel='noreferrer'
+      >
+        International Space Station 3D Model
+      </a>{' '}
+      by{' '}
+      <a href='https://www.nasa.gov/' target='_blank' rel='noreferrer'>
+        NASA
+      </a>
+      .
+    </div>
+  </>
 )
 
 export default Story
