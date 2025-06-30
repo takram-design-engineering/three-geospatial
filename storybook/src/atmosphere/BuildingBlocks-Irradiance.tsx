c60d24ef7c87d9ae7537f1a09ce0a615c946eca8
Shota Matsuda <shota@takram.com>
2025-07-01 04:51:25 +0900

    Add selector for the source of textures

--- storybook/src/atmosphere/BuildingBlocks-Irradiance.tsx
+++ storybook/src/atmosphere/BuildingBlocks-Irradiance.tsx
@@ -1,35 +1,62 @@
-import { Canvas, useLoader } from '@react-three/fiber'
+import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import { type StoryFn } from '@storybook/react-vite'
-import { Suspense, type FC } from 'react'
+import { useEffect, useMemo, type FC } from 'react'
 
-import { PrecomputedTexturesLoader } from '@takram/three-atmosphere'
+import {
+  PrecomputedTexturesGenerator,
+  PrecomputedTexturesLoader
+} from '@takram/three-atmosphere'
 
+import { useControls } from '../helpers/useControls'
 import { DataTextureViewer } from './helpers/DataTextureViewer'
 
+const binaryLoader = new PrecomputedTexturesLoader('binary')
+const exrLoader = new PrecomputedTexturesLoader('exr')
+
 const Content: FC = () => {
-  const textures = useLoader(
-    PrecomputedTexturesLoader,
-    'atmosphere',
-    loader => {
-      loader.format = 'binary'
+  const { source } = useControls({
+    source: {
+      options: ['generator', 'binary', 'exr'] as const
     }
+  })
+
+  const renderer = useThree(({ gl }) => gl)
+  const generator = useMemo(
+    () => new PrecomputedTexturesGenerator(renderer),
+    [renderer]
   )
+  useEffect(() => {
+    void generator.update()
+    return () => {
+      generator.dispose()
+    }
+  }, [generator])
+
+  binaryLoader.setType(renderer)
+  exrLoader.setType(renderer)
+  const textures = useLoader(
+    source === 'binary' ? binaryLoader : exrLoader,
+    'atmosphere'
+  )
+
   return (
-    <Canvas>
-      <DataTextureViewer
-        texture={textures.irradianceTexture}
-        fileName='irradiance.exr'
-        zoom={4}
-        valueScale={100}
-      />
-    </Canvas>
+    <DataTextureViewer
+      texture={
+        source === 'generator'
+          ? generator.textures.irradianceTexture
+          : textures.irradianceTexture
+      }
+      fileName='irradiance.exr'
+      zoom={4}
+      valueScale={100}
+    />
   )
 }
 
 const Story: StoryFn = () => (
-  <Suspense>
+  <Canvas>
     <Content />
-  </Suspense>
+  </Canvas>
 )
 
 export default Story
--- storybook/src/atmosphere/BuildingBlocks-Scattering.tsx
+++ storybook/src/atmosphere/BuildingBlocks-Scattering.tsx
@@ -1,34 +1,61 @@
-import { Canvas, useLoader } from '@react-three/fiber'
+import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import { type StoryFn } from '@storybook/react-vite'
-import { Suspense, type FC } from 'react'
+import { useEffect, useMemo, type FC } from 'react'
 
-import { PrecomputedTexturesLoader } from '@takram/three-atmosphere'
+import {
+  PrecomputedTexturesGenerator,
+  PrecomputedTexturesLoader
+} from '@takram/three-atmosphere'
 
+import { useControls } from '../helpers/useControls'
 import { Data3DTextureViewer } from './helpers/Data3DTextureViewer'
 
+const binaryLoader = new PrecomputedTexturesLoader('binary')
+const exrLoader = new PrecomputedTexturesLoader('exr')
+
 const Content: FC = () => {
-  const textures = useLoader(
-    PrecomputedTexturesLoader,
-    'atmosphere',
-    loader => {
-      loader.format = 'binary'
+  const { source } = useControls({
+    source: {
+      options: ['generator', 'binary', 'exr'] as const
     }
+  })
+
+  const renderer = useThree(({ gl }) => gl)
+  const generator = useMemo(
+    () => new PrecomputedTexturesGenerator(renderer),
+    [renderer]
   )
+  useEffect(() => {
+    void generator.update()
+    return () => {
+      generator.dispose()
+    }
+  }, [generator])
+
+  binaryLoader.setType(renderer)
+  exrLoader.setType(renderer)
+  const textures = useLoader(
+    source === 'binary' ? binaryLoader : exrLoader,
+    'atmosphere'
+  )
+
   return (
-    <Canvas>
-      <Data3DTextureViewer
-        texture={textures.scatteringTexture}
-        fileName='scattering.exr'
-        valueScale={0.5}
-      />
-    </Canvas>
+    <Data3DTextureViewer
+      texture={
+        source === 'generator'
+          ? generator.textures.scatteringTexture
+          : textures.scatteringTexture
+      }
+      fileName='scattering.exr'
+      valueScale={0.5}
+    />
   )
 }
 
 const Story: StoryFn = () => (
-  <Suspense>
+  <Canvas>
     <Content />
-  </Suspense>
+  </Canvas>
 )
 
 export default Story
