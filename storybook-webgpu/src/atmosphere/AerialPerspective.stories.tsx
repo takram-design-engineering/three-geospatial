88e72cfd1fdf6c296f7ab64e81e204bdd230ea24
Shota Matsuda <shota@takram.com>
2025-08-01 05:29:55 +0900

    Rename story

--- storybook-webgpu/src/atmosphere/AerialPerspective.stories.tsx
+++ storybook-webgpu/src/atmosphere/AerialPerspective.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta } from '@storybook/react-vite'
 
 import { createStory } from '../helpers/createStory'
-import { Story as WorldToECEFMatrixStory } from './AerialPerspective-WorldToECEFMatrix'
+import { Story as WorldOriginRebasingStory } from './AerialPerspective-WorldOriginRebasing'
 
 export default {
   title: 'atmosphere/Aerial Perspective'
 } satisfies Meta
 
-export const WorldToECEFMatrix = createStory(WorldToECEFMatrixStory)
+export const WorldOriginRebasing = createStory(WorldOriginRebasingStory)
