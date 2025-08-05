77ee4430c66793f4bf26f2829837ad6f89892337
Shota Matsuda <shota@takram.com>
2025-08-05 18:43:12 +0900

    Rename to scene background

--- storybook-webgpu/src/atmosphere/Sky.stories.tsx
+++ storybook-webgpu/src/atmosphere/Sky.stories.tsx
@@ -1,12 +1,12 @@
 import type { Meta } from '@storybook/react-vite'
 
 import { createStory } from '../helpers/createStory'
-import { Story as BackgroundStory } from './Sky-Background'
 import { Story as BasicStory } from './Sky-Basic'
+import { Story as SceneBackgroundStory } from './Sky-SceneBackground'
 
 export default {
   title: 'atmosphere/Sky'
 } satisfies Meta
 
 export const Basic = createStory(BasicStory)
-export const Background = createStory(BackgroundStory)
+export const SceneBackground = createStory(SceneBackgroundStory)
