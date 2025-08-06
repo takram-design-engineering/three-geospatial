30fd057ca11ddb9366e7d874d114c6d857d2ebe9
Shota Matsuda <shota@takram.com>
2025-08-07 07:26:41 +0900

    Remove Fnv workaround

--- packages/core/src/webgpu/Fnv.ts
+++ packages/core/src/webgpu/Fnv.ts
@@ -2,44 +2,10 @@ import type { ProxiedTuple, ShaderNodeFn } from 'three/src/nodes/TSL.js'
 import { Fn } from 'three/tsl'
 import type { NodeBuilder } from 'three/webgpu'
 
-const cache = new WeakMap()
-
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-class ObjectValue {}
-
-function createProxy<T extends Record<string | symbol, any>>(value: T): T {
-  return new Proxy(value, {
-    get: (_, property) => value[property],
-    getPrototypeOf: () => ObjectValue.prototype
-  })
-}
-
-// WORKAROUND: As of r178, "Fn" always expects the callback function to be an
-// object form if the type of the first argument is a plain object. This is not
-// a limitation of "Fnv" but "Fn" itself.
-// For example,
-//   Fn(([a, b]) => {})({ a: 1 }, 1)
-// doesn't work because "Fn" passes { a: 1 } instead of [{ a: 1 }, 1] to [a, b]
-// and causes a runtime error because { a: 1 } is not iterable.
-// This behavior can be bypassed when the prototype of the object in the first
-// argument is not Object.prototype.
-function workaround<T extends readonly unknown[]>(args: T): any {
-  const [value, ...rest] = args
-  if (value != null && Object.getPrototypeOf(value) === Object.prototype) {
-    let proxy = cache.get(value)
-    if (proxy == null) {
-      proxy = createProxy(value)
-      cache.set(value, proxy)
-    }
-    return [proxy, ...rest]
-  }
-  return args
-}
-
 type NonCallable<T> = T extends (...args: any[]) => any ? never : T
 
 export function Fnv<Args extends readonly unknown[], R>(
-  fn: (...args: Args) => (builder: NodeBuilder) => NonCallable<R>
+  fn: (...args: Args) => (builder: NodeBuilder) => R
 ): ShaderNodeFn<ProxiedTuple<Args>>
 
 export function Fnv<Args extends readonly unknown[], R>(
