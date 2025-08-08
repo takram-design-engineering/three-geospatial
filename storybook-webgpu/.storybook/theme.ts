a48d723e09e9a4b8355b545911b83eba59c4d514
Shota Matsuda <shota@takram.com>
2025-08-09 07:19:45 +0900

    Prefer Consolas for code

--- storybook-webgpu/.storybook/theme.ts
+++ storybook-webgpu/.storybook/theme.ts
@@ -1,7 +1,10 @@
 import { create } from 'storybook/theming'
 
+const base = create()
+
 export default create({
   base: 'dark',
   brandTitle: '@takram/three-geospatial Experimental WebGPU support',
-  brandUrl: 'https://github.com/takram-design-engineering/three-geospatial/'
+  brandUrl: 'https://github.com/takram-design-engineering/three-geospatial/',
+  fontCode: `Consolas, ${base.fontCode}`
 })
