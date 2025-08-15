78b6c5a3cddb14608a832605d33eb4294c25b945
Shota Matsuda <shota@takram.com>
2025-08-15 10:53:01 +0900

    Remove light from basic story

--- storybook-webgpu/src/atmosphere/AerialPerspective-Basic.tsx
+++ storybook-webgpu/src/atmosphere/AerialPerspective-Basic.tsx
@@ -1,4 +1,4 @@
-import { extend, useThree, type ThreeElement } from '@react-three/fiber'
+import { useThree } from '@react-three/fiber'
 import { CesiumIonAuthPlugin } from '3d-tiles-renderer/plugins'
 import {
   GlobeControls,
@@ -16,8 +16,6 @@ import {
 import { getSunDirectionECEF } from '@takram/three-atmosphere'
 import {
   aerialPerspective,
-  AtmosphereLight,
-  AtmosphereLightNode,
   atmosphereLUT,
   AtmosphereRenderingContext
 } from '@takram/three-atmosphere/webgpu'
@@ -50,14 +48,6 @@ import {
 import { useResource } from '../helpers/useResource'
 import { WebGPUCanvas } from '../helpers/WebGPUCanvas'
 import { TileMaterialReplacementPlugin } from '../plugins/TileMaterialReplacementPlugin'
-
-declare module '@react-three/fiber' {
-  interface ThreeElements {
-    atmosphereLight: ThreeElement<typeof AtmosphereLight>
-  }
-}
-
-extend({ AtmosphereLight })
 
 const Scene: FC<StoryProps> = ({
   longitude,
@@ -157,13 +147,7 @@ interface StoryProps extends PointOfViewProps {}
 interface StoryArgs extends OutputPassArgs, ToneMappingArgs, LocalDateArgs {}
 
 export const Story: StoryFC<StoryProps, StoryArgs> = props => (
-  <WebGPUCanvas
-    renderer={{
-      onInit: renderer => {
-        renderer.library.addLight(AtmosphereLightNode, AtmosphereLight)
-      }
-    }}
-  >
+  <WebGPUCanvas>
     <Scene {...props} />
   </WebGPUCanvas>
 )
