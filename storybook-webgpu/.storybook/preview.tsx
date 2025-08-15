0cf4a60ba0ba8dff0a07a38a9e287b3b2bc84ea5
Shota Matsuda <shota@takram.com>
2025-08-15 17:14:07 +0900

    Add further workaround to prevent storybook from storing args in url query

--- storybook-webgpu/.storybook/preview.tsx
+++ storybook-webgpu/.storybook/preview.tsx
@@ -1,6 +1,24 @@
 import type { Preview } from '@storybook/react-vite'
+import { Preview as PreviewClass } from 'storybook/preview-api'
 
 import './style.css'
+
+import { STORY_ARGS_UPDATED } from 'storybook/internal/core-events'
+
+// WORKAROUND: Prevent Storybook from storing args state in URL.
+const onUpdateArgs = PreviewClass.prototype.onUpdateArgs
+PreviewClass.prototype.onUpdateArgs = async function (...args) {
+  const channel = this.channel
+  this.channel = {
+    emit: (event, ...args) => {
+      if (event !== STORY_ARGS_UPDATED) {
+        channel.emit(event, ...args)
+      }
+    }
+  }
+  await onUpdateArgs.apply(this, args)
+  this.channel = channel
+}
 
 const preview: Preview = {
   parameters: {
