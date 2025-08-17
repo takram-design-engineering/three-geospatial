4d35fe8da1a2476a9a52b458c8bc9d5365905f94
Shota Matsuda <shota@takram.com>
2025-08-17 09:25:12 +0900

    Remove WritableProperties

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,5 +1,4 @@
 import type { FloatType, HalfFloatType, Uniform } from 'three'
-import type { Writable } from 'type-fest'
 
 export type Callable = (...args: any) => any
 
@@ -9,7 +8,3 @@ export type UniformMap<T> = Omit<Map<string, Uniform>, 'get'> & {
 }
 
 export type AnyFloatType = typeof FloatType | typeof HalfFloatType
-
-export type WritableProperties<T, U = Writable<T>> = {
-  [K in keyof U as U[K] extends Callable ? never : K]: U[K]
-}
