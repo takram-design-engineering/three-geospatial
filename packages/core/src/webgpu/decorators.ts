6abcd6d9a72b0bf7eafda199d00c87fcd1500ecd
Shota Matsuda <shota@takram.com>
2025-08-19 05:48:46 +0900

    Add referenceNode decorator

--- packages/core/src/webgpu/decorators.ts
+++ packages/core/src/webgpu/decorators.ts
@@ -1,7 +1,10 @@
+import { reference } from 'three/tsl'
+import type { Node } from 'three/webgpu'
 import invariant from 'tiny-invariant'
 
+import { assertType } from '../assertions'
 import { NODE_TYPES } from './internals'
-import type { NodeType } from './node'
+import type { NodeType, NodeValueType } from './node'
 
 // TODO: Use the stage 2 decorator with metadata support, but it will break
 // every other decorator.
@@ -29,6 +32,43 @@ export function nodeType(type: NodeType) {
     const nodeTypes = constructor[NODE_TYPES]
     invariant(nodeTypes != null)
     nodeTypes[propertyKey as string] = type
+  }
+}
+
+type InferPropertyKey<T> = T extends `${infer PropertyKey}Node`
+  ? PropertyKey['length'] extends 0
+    ? never
+    : PropertyKey
+  : never
+
+export function referenceNode(type: NodeType) {
+  return <
+    T extends Record<K, Node> & Record<InferPropertyKey<K>, NodeValueType>,
+    K extends keyof {
+      [K in keyof T as K extends string ? K : never]: unknown
+    }
+  >(
+    target: T,
+    propertyKey: K
+  ): void => {
+    assertType<string>(propertyKey)
+    invariant(propertyKey.length > 4)
+    invariant(propertyKey.endsWith('Node'))
+
+    let propertyValue
+    Object.defineProperty(target, propertyKey, {
+      enumerable: true,
+      get() {
+        return (propertyValue ??= reference(
+          propertyKey.slice(0, -4),
+          type,
+          this
+        ))
+      },
+      set(value) {
+        propertyValue = value
+      }
+    })
   }
 }
 
