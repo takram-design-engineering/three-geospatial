03d0c76bbc2dfbb4237b4a27ac7d82cf78d84acc
Shota Matsuda <shota@takram.com>
2025-08-25 11:21:04 +0900

    Derive GaussianBlurNode from SeparableFilterNode

--- packages/core/src/webgpu/GaussianBlurNode.ts
+++ packages/core/src/webgpu/GaussianBlurNode.ts
@@ -1,27 +1,10 @@
 import { GaussKernel } from 'postprocessing'
-import {
-  ClampToEdgeWrapping,
-  HalfFloatType,
-  LinearFilter,
-  RenderTarget,
-  RGBAFormat,
-  Vector2
-} from 'three'
-import { add, Fn, nodeObject, uniform, uv } from 'three/tsl'
-import {
-  NodeMaterial,
-  NodeUpdateType,
-  QuadMesh,
-  RendererUtils,
-  TempNode,
-  type NodeBuilder,
-  type NodeFrame,
-  type TextureNode
-} from 'three/webgpu'
+import { add, Fn, nodeObject, uv } from 'three/tsl'
+import type { Node, TextureNode } from 'three/webgpu'
 import invariant from 'tiny-invariant'
 
 import type { NodeObject } from './node'
-import { outputTexture } from './OutputTextureNode'
+import { SeparableFilterNode } from './SeparableFilterNode'
 
 declare module 'postprocessing' {
   interface GaussKernel {
@@ -34,111 +17,26 @@ declare module 'postprocessing' {
   }
 }
 
-function createRenderTarget(name: string): RenderTarget {
-  const renderTarget = new RenderTarget(1, 1, {
-    depthBuffer: false,
-    type: HalfFloatType,
-    format: RGBAFormat
-  })
-  const texture = renderTarget.texture
-  texture.minFilter = LinearFilter
-  texture.magFilter = LinearFilter
-  texture.wrapS = ClampToEdgeWrapping
-  texture.wrapT = ClampToEdgeWrapping
-  texture.generateMipmaps = false
-  texture.name = name
-  return renderTarget
-}
-
-let rendererState: RendererUtils.RendererState
-
-export class GaussianBlurNode extends TempNode {
+export class GaussianBlurNode extends SeparableFilterNode {
   static override get type(): string {
     return 'GaussianBlurNode'
   }
 
-  inputNode: TextureNode | null
-  iterations: number
-  resolutionScale = 0.5
-
   private readonly kernelSize: number
 
-  private readonly horizontalRT = createRenderTarget('GaussianBlur.Horizontal')
-  private readonly verticalRT = createRenderTarget('GaussianBlur.Vertical')
-  private readonly material = new NodeMaterial()
-  private readonly mesh = new QuadMesh(this.material)
-
-  private readonly texelSize = uniform(new Vector2())
-  private readonly direction = uniform(new Vector2())
-
-  // WORKAROUND: The leading underscore avoids infinite recursion.
-  // https://github.com/mrdoob/three.js/issues/31522
-  private readonly _textureNode: TextureNode
-
-  constructor(inputNode: TextureNode | null, kernelSize = 35, iterations = 1) {
-    super('vec4')
-    this.inputNode = inputNode
+  constructor(inputNode: TextureNode | null, kernelSize = 35) {
+    super(inputNode)
     this.kernelSize = kernelSize
-    this.iterations = iterations
-
-    this._textureNode = outputTexture(this, this.verticalRT.texture)
-
-    this.updateBeforeType = NodeUpdateType.FRAME
   }
 
-  getTextureNode(): TextureNode {
-    return this._textureNode
-  }
-
-  setSize(width: number, height: number): this {
-    const { resolutionScale } = this
-    const w = Math.max(Math.round(width * resolutionScale), 1)
-    const h = Math.max(Math.round(height * resolutionScale), 1)
-    this.horizontalRT.setSize(w, h)
-    this.verticalRT.setSize(w, h)
-    return this
-  }
-
-  override updateBefore({ renderer }: NodeFrame): void {
-    if (renderer == null) {
-      return
-    }
-    rendererState = RendererUtils.resetRendererState(renderer, rendererState)
-
-    const { horizontalRT, verticalRT, mesh, inputNode, direction } = this
-    invariant(inputNode != null)
-
-    const originalTexture = inputNode.value
-
-    const { width, height } = inputNode.value
-    this.setSize(width, height)
-
-    this.texelSize.value.set(1 / width, 1 / height)
-
-    for (let i = 0; i < this.iterations; ++i) {
-      direction.value.set(1, 0)
-      renderer.setRenderTarget(horizontalRT)
-      mesh.render(renderer)
-      inputNode.value = horizontalRT.texture
-
-      direction.value.set(0, 1)
-      renderer.setRenderTarget(verticalRT)
-      mesh.render(renderer)
-      inputNode.value = verticalRT.texture
-    }
-
-    RendererUtils.restoreRendererState(renderer, rendererState)
-
-    inputNode.value = originalTexture
-  }
-
-  override setup(builder: NodeBuilder): unknown {
+  protected setupFilterNode(): Node {
     const { inputNode, texelSize, direction } = this
     invariant(inputNode != null)
+
     // TODO: Remove the dependency to postprocessing.
     const kernel = new GaussKernel(this.kernelSize, 2)
 
-    const main = Fn(() => {
+    return Fn(() => {
       const center = uv()
       const offsetSize = direction.mul(texelSize).toVertexStage()
 
@@ -153,21 +51,7 @@ export class GaussianBlurNode extends TempNode {
         )
       }
       return result
-    })
-
-    const { material } = this
-    material.fragmentNode = main()
-    material.needsUpdate = true
-
-    this._textureNode.uvNode = inputNode.uvNode
-    return this._textureNode
-  }
-
-  override dispose(): void {
-    super.dispose()
-    this.horizontalRT.dispose()
-    this.verticalRT.dispose()
-    this.material.dispose()
+    })()
   }
 }
 
