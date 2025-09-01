0cbac1b0d0f66c9d51093f8bb68e26a624dd8a40
Shota Matsuda <shota@takram.com>
2025-09-02 02:17:11 +0900

    Fix UV

--- packages/core/src/QuadGeometry.ts
+++ packages/core/src/QuadGeometry.ts
@@ -7,7 +7,8 @@ export class QuadGeometry extends BufferGeometry {
     this.boundingSphere.set(new Vector3(), Infinity)
     this.setAttribute(
       'position',
-      new Float32BufferAttribute([-1, -1, 3, -1, -1, 3], 2)
+      new Float32BufferAttribute([-1, 3, 0, -1, -1, 0, 3, -1, 0], 3)
     )
+    this.setAttribute('uv', new Float32BufferAttribute([0, -1, 0, 1, 2, 1], 2))
   }
 }
