bcf214619d43033c7dc1c6c35d36323d24e02e4c
Shota Matsuda <shota@takram.com>
2025-09-05 03:45:59 +0900

    Dispose managed resources

--- storybook-webgpu/src/helpers/useResource.ts
+++ storybook-webgpu/src/helpers/useResource.ts
@@ -4,28 +4,32 @@ interface Resource {
   dispose: () => void
 }
 
+type ManageFunction = <R extends readonly Resource[]>(
+  ...resources: R
+) => R['length'] extends 1 ? R[0] : R
+
 export function useResource<T extends Resource | Resource[]>(
-  callback: (
-    manage: <R extends readonly Resource[]>(...resources: R) => R
-  ) => T,
+  callback: (manage: ManageFunction) => T,
   deps: readonly unknown[]
 ): T {
   const managedResourcesRef = useRef<Resource[]>([])
   const manage = useCallback((...resources: readonly Resource[]) => {
     managedResourcesRef.current.push(...resources)
-    return resources
-  }, []) as <R extends readonly Resource[]>(...resources: R) => R
+    return resources.length === 1 ? resources[0] : resources
+  }, []) as ManageFunction
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const resource = useMemo(() => callback(manage), deps)
 
   useEffect(() => {
     return () => {
-      if (Array.isArray(resource)) {
-        resource.forEach(resource => {
-          resource.dispose()
-        })
-      } else {
+      const resources = [
+        ...(Array.isArray(resource) ? resource : [resource]),
+        ...managedResourcesRef.current
+      ].filter((value, index, array) => array.indexOf(value) === index)
+      managedResourcesRef.current = []
+
+      for (const resource of resources) {
         resource.dispose()
       }
     }
