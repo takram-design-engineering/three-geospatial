aeb6d6d23bcb928e860cb9897961432298ee5da0
Shota Matsuda <shota@takram.com>
2025-10-05 13:20:04 +0900

    Fix gltf errors and rotate engine blades

--- storybook-webgpu/src/components/B787.tsx
+++ storybook-webgpu/src/components/B787.tsx
@@ -1,5 +1,8 @@
 import { useGLTF } from '@react-three/drei'
-import { useLayoutEffect, type ComponentProps, type FC } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useLayoutEffect, useMemo, type ComponentProps, type FC } from 'react'
+
+import { euclideanModulo } from '@takram/three-geospatial'
 
 export interface B787Props extends ComponentProps<'group'> {}
 
@@ -11,6 +14,25 @@ export const B787: FC<B787Props> = props => {
       mesh.castShadow = true
     })
   }, [gltf])
+
+  const { engines } = useMemo(() => {
+    const scene = gltf.scene
+    return {
+      engines: [
+        scene.getObjectByName('Engine_Left_63'),
+        scene.getObjectByName('Engine_Right_66')
+      ].filter(value => value != null)
+    }
+  }, [gltf.scene])
+
+  useFrame((state, delta) => {
+    const rpm = 8400
+    const tau = Math.PI * 2
+    let r = engines[0].rotation.z
+    r = euclideanModulo(r + (rpm / 60) * delta * tau, tau)
+    engines[0].rotation.z = r
+    engines[1].rotation.z = r
+  })
 
   return (
     <group {...props}>
