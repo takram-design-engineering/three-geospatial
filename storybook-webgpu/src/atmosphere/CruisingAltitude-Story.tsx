9b94fcdce5f9e3a9cbd103a585c78ec33a425c19
Shota Matsuda <shota@takram.com>
2025-10-06 09:52:19 +0900

    Add height and knots controls

--- storybook-webgpu/src/atmosphere/CruisingAltitude-Story.tsx
+++ storybook-webgpu/src/atmosphere/CruisingAltitude-Story.tsx
@@ -7,7 +7,7 @@ import {
 } from '@react-three/fiber'
 import { TilesPlugin } from '3d-tiles-renderer/r3f'
 import { Suspense, useRef, useState, type FC } from 'react'
-import { AgXToneMapping, Vector3 } from 'three'
+import { AgXToneMapping, Vector3, type Object3D } from 'three'
 import { mrt, output, pass, toneMapping, uniform } from 'three/tsl'
 import {
   MeshLambertNodeMaterial,
@@ -65,6 +65,7 @@ import {
 } from '../controls/toneMappingControls'
 import { useGuardedFrame } from '../hooks/useGuardedFrame'
 import { useResource } from '../hooks/useResource'
+import { useSpringControl } from '../hooks/useSpringControl'
 import { ReorientationPlugin } from '../plugins/ReorientationPlugin'
 
 declare module '@react-three/fiber' {
@@ -74,6 +75,8 @@ declare module '@react-three/fiber' {
 }
 
 extend({ AtmosphereLight })
+
+const KNOT_TO_SI = 0.5144444444
 
 const Content: FC<StoryProps> = () => {
   const renderer = useThree<Renderer>(({ gl }) => gl as any)
@@ -150,10 +153,10 @@ const Content: FC<StoryProps> = () => {
     longitude: radians(12.9425),
     latitude: radians(47.5529)
   })
-  const height = 10660
-  const speed = 238 // m/s
-  const heading = radians(130)
+  const motionHeight = useSpringControl(({ height }: StoryArgs) => height)
+  const motionKnots = useSpringControl(({ knots }: StoryArgs) => knots)
 
+  const modelRef = useRef<Object3D>(null)
   const geodetic = new Geodetic()
   const position = new Vector3()
   useFrame((state, delta) => {
@@ -168,8 +171,10 @@ const Content: FC<StoryProps> = () => {
     const M = (a * (1 - e2)) / W ** 3
     const N = a / W
 
+    const heading = radians(130)
+    const height = motionHeight.get()
     const theta = heading - Math.PI / 2
-    const ddt = speed * delta
+    const ddt = motionKnots.get() * KNOT_TO_SI * delta
     const dx = (ddt * Math.sin(theta)) / ((N + height) * Math.cos(latitude))
     const dy = (ddt * Math.cos(theta)) / (M + height)
     longitude += dx
@@ -185,6 +190,9 @@ const Content: FC<StoryProps> = () => {
       reorientationPlugin.lat = latitude
       reorientationPlugin.height = height
       reorientationPlugin.update()
+    }
+    if (modelRef.current != null) {
+      modelRef.current.rotation.y = -heading
     }
   })
 
@@ -223,7 +231,7 @@ const Content: FC<StoryProps> = () => {
       </atmosphereLight>
       <OrbitControls minDistance={45} maxDistance={1e5} />
       <Suspense>
-        <B787 rotation-y={-heading} />
+        <B787 ref={modelRef} />
       </Suspense>
       <Globe overrideMaterial={MeshLambertNodeMaterial}>
         <TilesPlugin
@@ -237,7 +245,10 @@ const Content: FC<StoryProps> = () => {
 
 interface StoryProps {}
 
-interface StoryArgs extends OutputPassArgs, ToneMappingArgs, LocalDateArgs {}
+interface StoryArgs extends OutputPassArgs, ToneMappingArgs, LocalDateArgs {
+  height: number
+  knots: number
+}
 
 export const Story: StoryFC<StoryProps, StoryArgs> = props => (
   <WebGPUCanvas
@@ -267,6 +278,8 @@ export const Story: StoryFC<StoryProps, StoryArgs> = props => (
 
 Story.args = {
   // https://www.flightaware.com/live/flight/QFA10/history/20250928/1105Z/EGLL/YPPH/tracklog
+  height: 10660,
+  knots: 479,
   ...localDateArgs({
     dayOfYear: 271,
     timeOfDay: 8.8447
@@ -279,6 +292,21 @@ Story.args = {
 }
 
 Story.argTypes = {
+  height: {
+    control: {
+      type: 'range',
+      min: 3000,
+      max: 13000
+    }
+  },
+  knots: {
+    name: 'knots',
+    control: {
+      type: 'range',
+      min: 0,
+      max: 666 // Mach 1
+    }
+  },
   ...localDateArgTypes(),
   ...toneMappingArgTypes(),
   ...outputPassArgTypes({
