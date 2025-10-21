21996d9e9e7451b6f199e6b97e3ea29d5c574183
Shota Matsuda <shota@takram.com>
2025-10-21 21:06:24 +0900

    Add RGB overlay mode

--- storybook-webgpu/src/helpers/VideoWaveform.ts
+++ storybook-webgpu/src/helpers/VideoWaveform.ts
@@ -3,14 +3,17 @@ import {
   BufferAttribute,
   InstancedBufferGeometry,
   Line,
-  Vector2,
-  type BufferGeometry,
-  type Camera,
-  type Group,
-  type Material,
-  type Scene
+  Vector2
 } from 'three'
-import { instanceIndex, luminance, vec2, vec3, vertexIndex } from 'three/tsl'
+import {
+  instanceIndex,
+  luminance,
+  select,
+  uniform,
+  vec2,
+  vec3,
+  vertexIndex
+} from 'three/tsl'
 import {
   LineBasicNodeMaterial,
   type NodeMaterial,
@@ -26,7 +29,15 @@ import {
 
 import type { VideoAnalysis } from './VideoAnalysis'
 
-export type VideoWaveformMode = 'luma' | 'red' | 'green' | 'blue'
+const modes = {
+  luma: { components: 1 },
+  red: { components: 1 },
+  green: { components: 1 },
+  blue: { components: 1 },
+  rgb: { components: 3 }
+} satisfies Record<string, { components: number }>
+
+export type VideoWaveformMode = keyof typeof modes
 
 export class VideoWaveform extends Line {
   declare geometry: InstancedBufferGeometry
@@ -35,7 +46,10 @@ export class VideoWaveform extends Line {
   source?: VideoAnalysis | null
   mode: VideoWaveformMode
 
+  gain = uniform(10)
+
   private prevMode?: VideoWaveformMode
+  private prevComponents?: number
   private readonly prevSize = new Vector2()
 
   constructor(source?: VideoAnalysis | null, mode: VideoWaveformMode = 'luma') {
@@ -51,50 +65,64 @@ export class VideoWaveform extends Line {
 
     this.material = new LineBasicNodeMaterial()
     this.material.transparent = true
-    this.material.opacity = 0.05
+    this.material.opacity = 1
     this.material.blending = AdditiveBlending
   }
 
   private updateMaterial(): void {
     invariant(this.source != null)
     const { colorBuffer, uvBuffer, size } = this.source
-    const index = instanceIndex.mul(size.x).add(vertexIndex)
-    const inputColor = colorBuffer.element(index)
-    const inputUV = uvBuffer.element(index)
+    const index = instanceIndex.mod(size.y).mul(size.x).add(vertexIndex)
+    const channel = instanceIndex.div(size.y).toVar()
+    const input = colorBuffer.element(index)
+    const uv = uvBuffer.element(index)
 
     let color: NodeObject<'vec3'>
     let y: NodeObject<'float'>
     switch (this.mode) {
       case 'luma':
-        color = hsv2rgb(vec3(rgb2hsv(inputColor).xy, 1))
-        y = luminance(inputColor)
+        color = hsv2rgb(vec3(rgb2hsv(input).xy, 1))
+        y = luminance(input)
         break
       case 'red':
-        color = vec3(1, 0.5, 0.5)
-        y = inputColor.r
+        color = vec3(1, 0.25, 0.25)
+        y = input.r
         break
       case 'green':
-        color = vec3(0.5, 1, 0.5)
-        y = inputColor.g
+        color = vec3(0.25, 1, 0.25)
+        y = input.g
         break
       case 'blue':
-        color = vec3(0.5, 0.5, 1)
-        y = inputColor.b
+        color = vec3(0.25, 0.25, 1)
+        y = input.b
+        break
+      case 'rgb':
+        color = select(
+          channel.equal(0),
+          vec3(1, 0.25, 0.25),
+          select(
+            channel.equal(1),
+            vec3(0.25, 1, 0.25),
+            select(channel.equal(2), vec3(0.25, 0.25, 1), 0)
+          )
+        )
+        y = select(
+          channel.equal(0),
+          input.r,
+          select(
+            channel.equal(1),
+            input.g,
+            select(channel.equal(2), input.b, 0)
+          )
+        )
         break
     }
 
-    this.material.positionNode = vec3(vec2(inputUV.x, y).sub(0.5))
-    this.material.colorNode = color.toVertexStage()
+    this.material.positionNode = vec3(vec2(uv.x, y).sub(0.5))
+    this.material.colorNode = color.div(size.y).mul(this.gain).toVertexStage()
   }
 
-  override onBeforeRender(
-    renderer: unknown,
-    scene: Scene,
-    camera: Camera,
-    geometry: BufferGeometry,
-    material: Material,
-    group: Group
-  ): void {
+  override onBeforeRender(renderer: unknown): void {
     if (this.source == null) {
       return
     }
@@ -106,13 +134,15 @@ export class VideoWaveform extends Line {
     }
 
     const size = this.source.size.value
-    if (!size.equals(this.prevSize)) {
+    const components = modes[this.mode].components
+    if (!size.equals(this.prevSize) || components !== this.prevComponents) {
       this.prevSize.copy(size)
+      this.prevComponents = components
       this.geometry.setAttribute(
         'position',
         new BufferAttribute(new Float32Array(size.x * 3), 3)
       )
-      this.geometry.instanceCount = size.y
+      this.geometry.instanceCount = size.y * components
     }
   }
 
