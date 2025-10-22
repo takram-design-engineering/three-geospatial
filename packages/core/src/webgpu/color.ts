0e4d8bb06fcc7c5d555713a3d17033e6191fd031
Shota Matsuda <shota@takram.com>
2025-10-22 20:29:41 +0900

    Add Rec 709 conversion

--- packages/core/src/webgpu/color.ts
+++ packages/core/src/webgpu/color.ts
@@ -1,4 +1,17 @@
-import { min, mul, select, sub, vec3, vec4 } from 'three/tsl'
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import {
+  add,
+  div,
+  min,
+  mul,
+  overloadingFn,
+  pow,
+  select,
+  sub,
+  vec3,
+  vec4
+} from 'three/tsl'
 
 import { FnLayout } from './FnLayout'
 
@@ -92,3 +105,63 @@ export const hsl2rgb = /*#__PURE__*/ FnLayout({
   const c = mul(2, hsl.z).sub(1).abs().oneMinus().mul(hsl.y)
   return rgb.sub(0.5).mul(c).add(hsl.z)
 })
+
+const linearToRec709_float = /*#__PURE__*/ FnLayout({
+  name: 'linearToRec709_float',
+  type: 'float',
+  inputs: [{ name: 'value', type: 'float' }]
+})(([value]) => {
+  return select(
+    value.lessThan(0.018),
+    mul(4.5, value),
+    mul(1.099, pow(value, 0.45)).sub(0.099)
+  )
+})
+
+export const linearToRec709_vec3 = /*#__PURE__*/ FnLayout({
+  name: 'linearToRec709_vec3',
+  type: 'vec3',
+  inputs: [{ name: 'color', type: 'vec3' }]
+})(([color]) => {
+  return vec3(
+    linearToRec709_float(color.r),
+    linearToRec709_float(color.g),
+    linearToRec709_float(color.b)
+  )
+})
+
+export const linearToRec709 = /*#__PURE__*/ overloadingFn([
+  // BUG: The returned type is order-dependent.
+  linearToRec709_vec3,
+  linearToRec709_float
+])
+
+const rec709ToLinear_float = /*#__PURE__*/ FnLayout({
+  name: 'rec709ToLinear_float',
+  type: 'float',
+  inputs: [{ name: 'value', type: 'float' }]
+})(([value]) => {
+  return select(
+    value.lessThan(0.081),
+    div(value, 4.5),
+    pow(add(value, 0.099).div(1.099), 1 / 0.45)
+  )
+})
+
+export const rec709ToLinear_vec3 = /*#__PURE__*/ FnLayout({
+  name: 'rec709ToLinear_vec3',
+  type: 'vec3',
+  inputs: [{ name: 'color', type: 'vec3' }]
+})(([color]) => {
+  return vec3(
+    rec709ToLinear_float(color.r),
+    rec709ToLinear_float(color.g),
+    rec709ToLinear_float(color.b)
+  )
+})
+
+export const rec709ToLinear = /*#__PURE__*/ overloadingFn([
+  // BUG: The returned type is order-dependent.
+  rec709ToLinear_vec3,
+  rec709ToLinear_float
+])
