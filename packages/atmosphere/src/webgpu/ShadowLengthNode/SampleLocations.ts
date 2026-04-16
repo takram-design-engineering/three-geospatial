e332ef305b023db84e072bb350e23b4e30202ed7
Shota Matsuda <shota@takram.com>
2026-04-16 12:12:13 +0900

    Add helper for sample locations

---
+++ packages/atmosphere/src/webgpu/ShadowLengthNode/SampleLocations.ts
@@ -0,0 +1,50 @@
+import { BufferAttribute, BufferGeometry, Points } from 'three'
+import { instanceIndex, screenSize, uvec2, vec3, vec4 } from 'three/tsl'
+import { PointsNodeMaterial, type NodeMaterial } from 'three/webgpu'
+
+import { isValidScreenLocation } from './common'
+import type { CoordinateNode } from './CoordinateNode'
+
+export class SampleLocations extends Points {
+  declare material: NodeMaterial
+
+  constructor(coordinateNode: CoordinateNode) {
+    const geometry = new BufferGeometry()
+    geometry.setAttribute(
+      'position',
+      new BufferAttribute(new Float32Array(3), 3)
+    )
+    geometry.drawRange.count = 1 // Force render points as instances
+
+    const material = new PointsNodeMaterial()
+    material.depthTest = false
+    material.depthWrite = false
+
+    const { maxSamplesInSlice, numEpipolarSlices } = coordinateNode
+    const sampleIndex = instanceIndex.mod(maxSamplesInSlice)
+    const sliceIndex = instanceIndex.div(maxSamplesInSlice)
+
+    const coordinate = coordinateNode
+      .getTextureNode()
+      .load(uvec2(sampleIndex, sliceIndex))
+      .toVertexStage()
+
+    const isValid = isValidScreenLocation(coordinate.xy, screenSize)
+
+    material.colorNode = isValid.select(vec3(1, 0, 0), vec3())
+    material.vertexNode = isValid.select(
+      vec4(coordinate.xy, 0, 1),
+      vec4(2, 2, 0, 1)
+    )
+
+    super(geometry, material)
+
+    this.count = maxSamplesInSlice * numEpipolarSlices
+    this.frustumCulled = false
+  }
+
+  dispose(): void {
+    this.geometry.dispose()
+    this.material.dispose()
+  }
+}
